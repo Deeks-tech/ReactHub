371a246fb1fa98f547efb43f0f2c5bbc26f02f38
Deeks-tech <114800058+Deeks-tech@users.noreply.github.com>
2024-06-07 12:15:51 +0100

    new component add

--- ReactHub/src/App.jsx
+++ ReactHub/src/App.jsx
@@ -2,24 +2,39 @@ import "./App.css";
 import Header from "./components/Header";
 import FetchQueryExample from "./components/FetchExample/FetchQueryExample";
 import ContextExample from "./components/ContextExample/ContextExample";
+import RenderList from "./components/RenderLists/RenderList";
+import RenderList2 from "./components/RenderLists/RenderList2";
+import FetchWithAsync from "./components/FetchExample/FetchWithAsync";
+import data from "../db.json";
 import { createContext } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import CounterExample from "./components/CounterExample/CounterExample";
+import IteratingList from "./components/IteratingListExample/IteratingList";
+import OpenClose from "./components/OpenClose/OpenClose";
 
 const queryClient = new QueryClient();
 
-export const VariableToPassContext = createContext();
-
 const variableToPass = "Hello World";
 
+const passMore = "Hello Universe";
+
+export const VariableToPassContext = createContext();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Header />
       <FetchQueryExample />
 
-      <VariableToPassContext.Provider value={variableToPass}>
+      <VariableToPassContext.Provider value={{ variableToPass, passMore }}>
         <ContextExample />
       </VariableToPassContext.Provider>
+
+      <RenderList data={data} />
+      <RenderList2 />
+      <FetchWithAsync />
+      <CounterExample />
+      <IteratingList />
+      <OpenClose />
     </QueryClientProvider>
   );
 }
