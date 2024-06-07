359cd89843e37659ee9b68794d29df95cea21626
Deeks-tech <114800058+Deeks-tech@users.noreply.github.com>
2024-06-07 12:16:57 +0100

    Open/Close toggle add

---
+++ ReactHub/src/components/OpenClose/OpenClose.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+export default function OpenClose() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleOpen() {
+    setIsOpen(!isOpen);
+  }
+  return (
+    <section>
+      <div>
+        <h2>Open Close toggle example</h2>
+        <p>
+          This example uses a couple of different concepts , useState and
+          conditional rendering{" "}
+        </p>
+        <p>
+          {" "}
+          An <code>isOpen</code> state is set to false and when the button is
+          clicked the handle function sets state to the opposite using{" "}
+          <code>!isOpen</code>
+        </p>
+        <p>
+          using the conditonal rendering practice of <code>&&</code> we have
+          said that if isOpen is true then display the Hello world section , if
+          false display nothing.
+        </p>
+      </div>
+      {isOpen ? (
+        <button onClick={handleOpen}>Close</button>
+      ) : (
+        <button onClick={handleOpen}>open</button>
+      )}
+      <div>
+        {isOpen && (
+          <section>
+            <h3 className="open-header">Hello World 😎</h3>
+            <div>
+              <p></p>
+            </div>
+          </section>
+        )}
+      </div>
+    </section>
+  );
+}
