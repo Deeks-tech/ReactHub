51e5643d0ad78d435a0cfadbc20db56d484c7a87
Deeks-tech <114800058+Deeks-tech@users.noreply.github.com>
2024-06-11 12:44:30 +0100

    updates to content

--- ReactHub/src/components/Header.jsx
+++ ReactHub/src/components/Header.jsx
@@ -2,6 +2,11 @@ export default function Header() {
   return (
     <div>
       <h1 className="header-title">ReactHub</h1>
+      <p>
+        Some components in this app will have Vitest or Cypress tests added
+        these will be marked
+      </p>
+      <p>The app as a whole has a cypress test e2e spec file</p>
     </div>
   );
 }
