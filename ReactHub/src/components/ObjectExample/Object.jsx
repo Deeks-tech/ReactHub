a5c54f00a971cf29fee989ad033ee3a3f9374bf9
Deeks-tech <114800058+Deeks-tech@users.noreply.github.com>
2024-06-25 17:46:20 +0100

    access new info from object

--- ReactHub/src/components/ObjectExample/Object.jsx
+++ ReactHub/src/components/ObjectExample/Object.jsx
@@ -3,9 +3,8 @@ import data from "./objectData";
 export default function Object() {
   return (
     <section>
-      <div>
-        <>{data.id}</>
-      </div>
+      <div>{data.id}</div>
+      <div>{data.employmentDetails.hireDate}</div>
     </section>
   );
 }
